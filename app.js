ec38133f2dae93b10b80a029c3e2f825e5fdd838
JemyGer <167636081+JemyGer@users.noreply.github.com>
2024-09-25 03:36:56 +0200

    add
    URIMethod()

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
-let uri = "https://w3schools.com/my test.asp?name=ståle&car=saab";
-let encoded = encodeURIComponent(uri);
-let decoded = decodeURIComponent(encoded);
+// Method 1: Uri encoding
 
-console.log("Encoded URI: " + encoded);
-console.log("Decoded URI: " + decoded);
+function encodeExample() {
+  let uri = "https://w3schools.com/my test.asp?name=ståle&car=saab";
+  let encoded = encodeURIComponent(uri);
+  document.getElementById("demo").innerHTML = "Encode URI" + encoded;
+  console.log("Encoded URI:" + encoded);
+}
 
-document.getElementById("demo").innerHTML =
-  "Encoded URI:<br>" + encoded + "<br><br>" + "Decoded URI:<br>" + decoded;
+// Method 2: Uri decoding
+function decodeExample() {
+  let encoded =
+    "https%3A%2F%2Fw3schools.com%2Fmy%20test.asp%3Fname%3Dst%C3%A5le%26car%3Dsaab";
+  let decoded = decodeURIComponent(encoded);
+  document.getElementById("demo").innerHTML = "Decoded URI:" + decoded;
+  console.log("Decoded URI:" + decoded);
+}
+
+// Method 3: Custom Method
+function customMethod() {
+  document.getElementById("demo").innerHTML =
+    "Dies ist eine Benutzerdefinierte Methode.";
+  console.log("Custom Method executed!");
+}
